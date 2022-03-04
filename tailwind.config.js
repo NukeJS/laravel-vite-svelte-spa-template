6032cd23d8f8d314dfc50b9776db50a11490451f
ronniew <rwaebers@gmail.com>
2022-03-04 22:50:12 +0100

    Add `@tailwindcss/forms` and `@tailwindcss/aspect-ratio`

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,8 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/aspect-ratio"),
+    ],
 };
