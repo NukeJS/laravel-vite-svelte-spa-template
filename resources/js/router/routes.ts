39729b817a3ea5523a13d1190af8b2ba270ae503
ronniew <rwaebers@gmail.com>
2022-03-05 23:43:43 +0100

    Add 404 route

--- resources/js/router/routes.ts
+++ resources/js/router/routes.ts
@@ -2,6 +2,11 @@ import type { RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
     {
+        path: "/:pathMatch(.*)*",
+        name: "NotFound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+    {
         path: "/",
         name: "Home",
         component: () => import("@/views/Home.vue"),
