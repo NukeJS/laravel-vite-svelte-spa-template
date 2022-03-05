08c40f779596e9c13182a73a1421fa4be43d3d9e
ronniew <rwaebers@gmail.com>
2022-03-05 23:54:10 +0100

    rename variable

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import { fileURLToPath, URL } from "url";
 import Vue from "@vitejs/plugin-vue";
 
 export default defineConfig(({ command }) => {
-    const DEV = command === "serve";
+    const IS_DEV = command === "serve";
 
     return {
-        base: DEV ? "" : "/build/",
+        base: IS_DEV ? "" : "/build/",
         publicDir: "non_existent_folder",
         build: {
             outDir: "public/build",
